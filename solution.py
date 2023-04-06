318d70ad90becb1e6d8ecf1b7e1d633e61b6392b
eahnn <103137060+eahnn@users.noreply.github.com>
2023-04-06 20:41:30 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,8 +10,8 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * uniform.ppf(1 - alpha / 2), \
-           loc - scale * uniform.ppf(alpha / 2)
+    alpha = (1 - p) / 2
+    loc = 2 * x.mean()
+    scale = 2 * np.sqrt(np.var(x)) / np.sqrt(len(x))
+    return loc - scale * uniform.ppf(alpha), \
+           loc - scale * uniform.ppf(1 - alpha)
